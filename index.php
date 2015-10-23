da630695570d61dd0320fdd175021d857058bf9c
vinird <mvrd_17@hotmail.com>
2015-10-22 21:08:09 -0600

    Vnicio: se agrega la estructura general

---
+++ index.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<title>index</title>
+	<meta http-equiv='REFRESH' content='0;url=php/masterPage.php'>
+</head>
+</html>"
---
+++ php/masterPage.php
@@ -0,0 +1,194 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>master</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-sx-12 col-sm-8 col-md-6 col-lg-4" style="background:gray;">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, unde.</p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-sx-12 col-sm-8 col-md-6 col-lg-4 col-lg-offset-8 col-md-offset-6 col-sm-offset-4" style="background:#3464B8;">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, unde.</p>
+				</div>
+			</div>
+			<div class="row">
+				<nav>
+					<div class="col-sx-1 col-sm-1 col-md-1 col-lg-1"><a href="#">Home</a></div>
+					<div class="col-sx-1 col-sm-1 col-md-1 col-lg-1"><a href="#">Home</a></div>
+					<div class="col-sx-1 col-sm-1 col-md-1 col-lg-1"><a href="#">Home</a></div>
+					<div class="col-sx-1 col-sm-1 col-md-1 col-lg-1"><a href="#">Home</a></div>
+					<div class="col-sx-1 col-sm-1 col-md-1 col-lg-1"><a href="#">Home</a></div>
+					<div class="col-sx-1 col-sm-1 col-md-1 col-lg-1"><a href="#">Home</a></div>
+					<div class="col-sx-1 col-sm-1 col-md-1 col-lg-1"><a href="#">Home</a></div>
+				</nav>
+			</div>
+		</div>
+	</header>
+	<main>
+		<div class="container">
+			<div class="col-xs-12 col-sm-12 col-md-9 col-lg-9">
+				<div class="tabla">
+					<table class="table table-responsive table-striped table-hover table-bordered">
+						<thead>
+							<tr>
+								<th>Info</th>
+								<th>Info</th>
+								<th>Info</th>
+								<th>Info</th>
+								<th>Info</th>
+								<th>Info</th>
+								<th>Info</th>
+								<th>Info</th>
+								<th>Info</th>
+								<th>Info</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+							</tr>
+							<tr>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+							</tr>
+							<tr>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+							<tr>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+								<td></td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<div class="tabla-acciones">
+					
+				</div>
+			</div>
+			<div class="col-xs-12 col-sm-12 col-md-3 col-lg-3">
+				<aside>
+					
+				</aside>
+			</div>
+		</div>
+			<div class="clearfix"></div>
+	</main>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-sx-12 col-sm-6">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque, odit.</p>
+				</div>
+				<div class="col-sx-12 col-sm-6">
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, id.</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+<script src="../js/bootstrap.min.js"></script>
+</html>
