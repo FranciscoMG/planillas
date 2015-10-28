740c6d6d0d6e1eb00234b933db1c04ceef096b30
vinird <mvrd_17@hotmail.com>
2015-10-27 23:21:32 -0600

    Vinicio: se agregan asides, estilos a tabla y prueba modal

--- php/masterPage.php
+++ php/masterPage.php
@@ -9,8 +9,10 @@
     <link rel="stylesheet" href="../css/style.css">
 </head>
 <body>
+
+<!-- //////// Header /////////////////////////////////////////////////////////// -->
 	<header >
-		<div class="container">
+		<div class="container ">
 			<div id="logo-ucr" class="row">
 				<div  class="col-sx-12 col-sm-8 col-md-6 col-lg-4">
 					<img id="logo-ucr" src="../img/logo-ucr.png" class="img-responsive" alt="">
@@ -22,7 +24,7 @@
 				<div class="container">
 					<div  class="pull-right" style="">
 						<div class="dropdown">
- 							<button class="btn boton-perfil btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Perfil: Administrativo<span class="caret"></span></button>
+ 							<button class="btn boton-perfil btn-primary dropdown-toggle" type="button" data-toggle="dropdown">Perfil: Administrativo <span class="caret"></span></button>
  							<ul class="dropdown-menu">
 	 							<li class="disabled"><a href="#">Fainix Mayorga Solorzano</a></li>
 	 							<li><a href="#">Cambiar usuario</a></li>
@@ -120,11 +122,15 @@
 			</div>
 		</div>
 	</header>
+
+<!-- //////// Main /////////////////////////////////////////////////////////// -->
 	<main>
+
+<!--************* Tabla principal ********************-->
 		<div class="container" id="contenedor-tabla">
 			<a onclick="cambiarTable()" class="btn btn-lg pull-right"><span id="boton-tamano-tabla"class="glyphicon glyphicon-resize-full"  ></span></a>
-			<div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-				<div class="tabla table-responsive">
+			<div class="contenedor-tabla-2 col-xs-12 col-sm-12 col-md-12 col-lg-12">
+				<div class="tabla table-responsive contenedor-tabla-1">
 					<table class="table table-striped table-hover text-center">
 						<thead>
 							<tr>
@@ -142,6 +148,42 @@
 						</thead>
 						<tbody>
 							<tr>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+								<td>Datos 1</td>
+							</tr>
+							<tr>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+							</tr>
+							<tr>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+							</tr>
+							<tr>
 								<td>Datos</td>
 								<td>Datos</td>
 								<td>Datos</td>
@@ -178,88 +220,236 @@
 								<td>Datos</td>
 							</tr>
 							<tr>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
 							</tr>
 							<tr>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
 							</tr>
 							<tr>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
 							</tr>
 							<tr>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
 							</tr>
 							<tr>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
 							</tr>
 							<tr>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
-								<td></td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
 							</tr>
+							<tr>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+							</tr>
+							<tr>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+								<td>Datos</td>
+							</tr>
+
 						</tbody>
 					</table>
 				</div>
-				<div class="tabla-acciones">
-				
+				<br><br>
+					<div class="row">
+					<div class="col-xs-8 col-sm-6">
+						<div class="form-group">
+							  <label for="sel1">Carrera:</label>
+							  <select class="form-control" id="sel1">
+							    <option>1</option>
+							    <option>2</option>
+							    <option>3</option>
+							    <option>4</option>
+							  </select>
+						</div>
+						</div>
+						<div class="clearfix">
+							<a href="#" class="btn btn-info btn-sm">Aplicar</a>
+							<a href="#" class="btn btn-danger btn-sm pull-right" data-toggle="modal" data-target="#modalRevision">Revisión</a>
+
+						</div>
+					</div>
+			</div>
+		</div>
+
+<!--************* Aside ********************-->
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+					<div class="aside col-lg-10 col-lg-offset-1 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 col-sx-12">
+						<h4>Ultima revisión</h4>
+						<div class="separador-horizontal"></div>
+							<div class="table table-responsive">
+								<table class="table table-striped">
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+								</table>
+							</div>
+
+					</div>
 				</div>
+				<div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+					<div class="aside col-lg-10 col-lg-offset-1 col-md-offset-1 col-md-10 col-sm-offset-1 col-sm-10 col-sx-12">
+						<h4>Comentarios</h4>
+						<div class="separador-horizontal"></div>
+							<div class="table table-responsive">
+								<table class="table table-striped">
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+									<tr>
+										<td>Datos</td>
+									</tr>
+								</table>
+							</div>
+
+					</div>
 			</div>
 		</div>
 
 		<div class="clearfix"></div>
 	</main>
+
+<!-- //////// Footer /////////////////////////////////////////////////////////// -->
 	<footer class="footer" >
 		<div class="container-fluid">
 			<div class="row row1">
@@ -277,6 +467,28 @@
 			</div>
 		</div>
 	</footer>
+
+	<!-- Modal -->
+<div id="modalRevision" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
 </body>
 <script src="../js/jquery-1.11.3.min.js"></script>
 <script src="../js/bootstrap.min.js"></script>
