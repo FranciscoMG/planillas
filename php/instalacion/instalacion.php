fd62a098bf25dad4b80098c86b0827131def3925
oto80557 <hectorsalasarias@gmail.com>
2015-11-08 18:21:09 -0600

    X de los modales
    Agregadas las "X"

--- php/instalacion/instalacion.php
+++ php/instalacion/instalacion.php
@@ -1,5 +1,5 @@
 <?php
-  mysql_connect("localhost","root","maikol") or die ("Error al realizar la conexión inicial: ".mysql_error());
+  mysql_connect("localhost","root","") or die ("Error al realizar la conexión inicial: ".mysql_error());
 
   mysql_query("CREATE DATABASE SIDOP;") or die ("Error a crear la base de datos: ".mysql_error());
   mysql_select_db("SIDOP");
@@ -16,6 +16,6 @@
   mysql_query("GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, INDEX, ALTER, SUPER, CREATE TEMPORARY TABLES, EXECUTE, CREATE VIEW, SHOW VIEW, CREATE ROUTINE, ALTER ROUTINE, EVENT, TRIGGER ON *.* TO 'admin_db'@'localhost' IDENTIFIED BY PASSWORD '*FCF9D342730D26CEEDE3BF8E23DF7040CC0BA26B';") or die ("Error al crear el usuario".mysql_error());
 
   mysql_query("INSERT INTO `SIDOP`.`tb_Usuario` (`usuario`, `contrasena`, `nombre_usuario`, `apellido_usuario`, `perfil`, `correo_usuario`, `habilitado`) VALUES ('administrador', '123', 'pedro', 'rodriguez', '1', 'asdad@asd.com', '1');") or die ("Error al insertar el primer usuaio".mysql_error());
-  
+
   echo ("La base fue creada correctamente.");
 ?>
